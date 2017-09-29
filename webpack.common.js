dee523d533e5e6807e64745e5369d37d0514b32b
fgeorgy <francois.georgy@gmail.com>
2017-09-29 17:31:39 +0200

    remove unused code

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,9 @@
-// const webpack = require("webpack");
 const path = require('path');
 
 module.exports = {
     entry: [
         './src/svg-knob.js'
     ],
-    // plugins: [
-    //     new webpack.ProvidePlugin({
-    //         $: "jquery",
-    //         jQuery: "jquery",
-    //         "window.jQuery": "jquery'",
-    //         "window.$": "jquery"
-    //     }),
-    // ],
     output: {
         filename: './svg-knob.js',
         path: path.resolve(__dirname, 'dist')
